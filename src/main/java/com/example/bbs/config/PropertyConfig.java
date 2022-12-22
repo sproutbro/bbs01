7e1472d52f6b43681f736db9aae06d185fee273c
guts <krsproutbro@gmail.com>
2022-12-23 04:34:26 +0900

    복구함

---
+++ src/main/java/com/example/bbs/config/PropertyConfig.java
@@ -0,0 +1,10 @@
+package com.example.bbs.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({@PropertySource("classpath:env.properties")})
+public class PropertyConfig {
+}
